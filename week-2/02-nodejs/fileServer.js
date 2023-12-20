bc9d3aca20e0002667691d6481aa199de06e75d9
hardikk kamboj <kambojpushkal@gmail.com>
2023-12-20 22:23:52 +0530

    fileserver sol

--- week-2/02-nodejs/fileServer.js
+++ week-2/02-nodejs/fileServer.js
@@ -17,5 +17,36 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+// 1. GET /files - Returns a list of files present in `./files/` directory
+// Response: 200 OK with an array of file names in JSON format.
+// Example: GET http://localhost:3000/files
+
+app.get('/files', function (req, res) {
+  fs.readdir(path.join(__dirname, './files/'), (err, files) => {
+  if (err) {
+      return res.status(500).json({ error: 'Failed to retrieve files' });
+  }
+  res.json(files);
+  });
+});
+
+
+app.get('/file/:filename', (req, res) => {
+    const filename = req.params.filename;
+    const filepath = path.join(__dirname, './files/', filename);
+    fs.readFile(filepath, 'utf8', (err, data) => {
+      if (err) {
+          return res.status(404).send('File not found');
+      }
+      res.status(200).send(data);
+      });
+}
+);
+
+app.all('*', (req, res) => {
+  res.status(404).send('Route not found');
+});
+
+
 
 module.exports = app;
